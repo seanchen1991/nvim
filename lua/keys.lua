74311d3f6176ea1f650af8cb5bc1ec68b67aafe1
Sean Chen <seanchen11235@gmail.com>
2022-10-07 09:12:25 -0500

    Add key map for quitting all open windows and tabs

--- lua/keys.lua
+++ lua/keys.lua
@@ -13,7 +13,7 @@ map('n', '<leader>pp', '<cmd>Telescope projects<cr>', {})
 
 -- Better commenting mappings
 map('n', 'gc', '<cmd>CommentToggle<cr>', {})
-map('v', 'gc', ':<cmd>CommentToggle<cr>', {})
+map('v', 'gc', '<cmd>CommentToggle<cr>', {})
 
 -- Map <C-w> to <leader><leader>
 map('n', '<leader><leader>', '<C-w>', {})
@@ -30,7 +30,7 @@ map('n', '<S-h>', '<cmd>bprevious<cr>', {})
 map('n', '<leader>md', '<cmd>Glow<cr>', {})
 
 -- Mappings for Hop
-map('n', '<leader>hw', '<cmd>HopWord<cr>', {})
+map('n', '<leader>3', '<cmd>HopWord<cr>', {})
 map('n', '<leader>1', '<cmd>HopChar1<cr>', {})
 map('n', '<leader>2', '<cmd>HopChar2<cr>', {})
 
@@ -39,3 +39,6 @@ map('n', '<leader>bc', '<cmd>bclose<cr>', {})
 
 -- Remap splitting a window to the right
 map('n', '<leader>sl', '<C-w><C-v>', {})
+
+-- Remap quit all loaded and open buffers, splits, and tabs
+map('n', '<leader>qq', '<cmd>wqall<cr>', {})
